74fcd3f92253b53f89b957a846c55a8a5cb38ff9
ttelcl <9429819+ttelcl@users.noreply.github.com>
2023-03-23 16:57:24 +0200

    LcGitLib: introducing GitId, and RefTree

---
+++ LcGitLib/GitModels/Refs/RefTree.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * (c) 2023  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LcGitLib.GitModels.Refs
+{
+  /// <summary>
+  /// A tree of GIT object refs. This is itself a RefTreeNode named "refs",
+  /// with no parent
+  /// </summary>
+  public class RefTree: RefTreeNode
+  {
+    /// <summary>
+    /// Create a new RefTree
+    /// </summary>
+    public RefTree()
+      : base("refs", null)
+    {
+    }
+
+    /// <summary>
+    /// Get or set the GitId value of a ref
+    /// </summary>
+    /// <param name="path">
+    /// The path identifying the ref
+    /// </param>
+    /// <returns>
+    /// The GitId of the node that was found (possibly null), or null if the
+    /// ref node wasn't found.
+    /// </returns>
+    public GitId? this[string path] {
+      get => GetNode(path, false)?.Value;
+      set {
+        GetNode(path, true).Value = value;
+      }
+    }
+
+    /// <summary>
+    /// Get or create a node in the tree, given its full path
+    /// </summary>
+    /// <param name="path">
+    /// The full path of the node, which must start with "refs/"
+    /// </param>
+    /// <param name="create">
+    /// If true: the node is created and inserted if it does not yet exist.
+    /// If false (default): the node is not create if it does not exist; null is returned instead
+    /// </param>
+    /// <returns>
+    /// The node that was found or created, or null if not found and <paramref name="create"/>
+    /// was false.
+    /// </returns>
+    public RefTreeNode GetNode(string path, bool create = false)
+    {
+      if(String.IsNullOrEmpty(path))
+      {
+        throw new ArgumentOutOfRangeException(
+          nameof(path), "Path cannot be empty");
+      }
+      var parts = path.Split(new char[] { '/' });
+      if(parts[0] != "refs")
+      {
+        // Technically speaking that error message is not correct, but 
+        // "must start with 'refs/' or be exactly equal to 'refs'"
+        // sounds overly convoluted.
+        throw new ArgumentOutOfRangeException(
+          nameof(path), "Path must start with 'refs/'");
+      }
+      RefTreeNode node = this;
+      foreach(var part in parts.Skip(1))
+      {
+        var child = node.GetChild(part, create);
+        if(child == null)
+        {
+          return null;
+        }
+        node = child;
+      }
+      return node;
+    }
+
+    /// <summary>
+    /// Enumerate all refs in the tree that have been assigned a value
+    /// </summary>
+    public IEnumerable<RefTreeNode> EnumerateAllValuedNodes()
+    {
+      return
+        EnumerateNodes()
+        .Where(n => n.Value != null);
+    }
+
+  }
+}
---
+++ LcGitLib/GitModels/Refs/RefTreeNode.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * (c) 2023  ttelcl / ttelcl
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LcGitLib.GitModels.Refs
+{
+  /// <summary>
+  /// A node in a namespace tree of refs
+  /// </summary>
+  public class RefTreeNode
+  {
+    private readonly Dictionary<string, RefTreeNode> _children;
+
+    /// <summary>
+    /// Create a new RefTreeNode
+    /// </summary>
+    internal RefTreeNode(string shortName, RefTreeNode parent)
+    {
+      if(String.IsNullOrEmpty(shortName))
+      {
+        throw new ArgumentOutOfRangeException(nameof(shortName), "A ref path segment name cannot be empty");
+      }
+      if(shortName.Contains('/'))
+      {
+        throw new ArgumentOutOfRangeException(nameof(shortName), "A ref path segment name cannot contain '/'");
+      }
+      Parent = parent;
+      ShortName = shortName;
+      _children = new Dictionary<string, RefTreeNode>();
+      if(parent == null)
+      {
+        FullName = shortName;
+      }
+      else
+      {
+        FullName = parent.FullName + "/" + shortName;
+      }
+    }
+
+    /// <summary>
+    /// The parent node (possibly null)
+    /// </summary>
+    public RefTreeNode Parent { get; init; }
+
+    /// <summary>
+    /// The short name of this node (path excluded)
+    /// </summary>
+    public string ShortName { get; init; }
+
+    /// <summary>
+    /// The full name of the node
+    /// </summary>
+    public string FullName { get; init; }
+
+    /// <summary>
+    /// Get or set the target object id, or null if there is none
+    /// </summary>
+    public GitId? Value { get; set; }
+
+    /// <summary>
+    /// Retrieve an existing direct child node or optionally create a new one
+    /// </summary>
+    /// <param name="name">
+    /// The child node's name
+    /// </param>
+    /// <param name="create">
+    /// Whether or not to create the node if it didn't exist yet.
+    /// Default false.
+    /// </param>
+    /// <returns>
+    /// The existing node, or the new node if it was created, or null
+    /// if the node did not exist and <paramref name="create"/> was false.
+    /// </returns>
+    public RefTreeNode GetChild(string name, bool create = false)
+    {
+      if(_children.TryGetValue(name, out RefTreeNode child))
+      {
+        return child;
+      }
+      else
+      {
+        if(create)
+        {
+          var node = new RefTreeNode(name, this);
+          _children.Add(name, node);
+          return node;
+        }
+        else
+        {
+          return null;
+        }
+      }
+    }
+
+    /// <summary>
+    /// The children of this node
+    /// </summary>
+    public IReadOnlyCollection<RefTreeNode> Children { get => _children.Values; }
+
+    /// <summary>
+    /// Enumerate this and all descencent nodes
+    /// </summary>
+    public IEnumerable<RefTreeNode> EnumerateNodes()
+    {
+      yield return this;
+      foreach(var node in Children.SelectMany(n => n.EnumerateNodes()))
+      {
+        yield return node;
+      }
+    }
+  }
+}
